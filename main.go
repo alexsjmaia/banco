210dcbc70cd6636b727328a5f0d075146d071af2
alexsjmaia <alexsjmaia@gmail.com>
2025-03-13 08:09:27 -0300

    Implementada a função Sacar

--- main.go
+++ main.go
@@ -9,8 +9,27 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
-func main() {
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 
-	fmt.Println(ContaCorrente{})
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque relizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func main() {
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+
+	fmt.Printf("%.2f\n", contaDaSilvia.saldo)
+
+	valorDoSaque := 499.99
+	fmt.Println(contaDaSilvia.Sacar(float64(valorDoSaque)))
+
+	fmt.Printf("Saldo Atual R$ %.2f\n", contaDaSilvia.saldo)
 
 }
